7b113a9aafb50200824d7014d9a9172bcc5f81f4
shob <shobsbot@gmail.com>
2025-10-21 22:45:50 -0500

    started with a list of things to learn

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+// TODO
+// [] find out how to do color arithmetic
+//  [] complements
+//  [] analogous
+//  [] monochrome
+//  [] split
+//  [] square
+//  [] triadic
+// [] translate color types
+//  [] rgb to hex
+//  [] hsl to hex
+//  [] name to hex?
+//  [] hsb to hex
+fn main() {
+    println!("Hello, world!");
+}
