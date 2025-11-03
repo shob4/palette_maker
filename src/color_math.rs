d7d418ae3755be07dab926257093268871970e5a
shob <shobsbot@gmail.com>
2025-11-03 13:44:09 -0600

    added rgb to hsl and test

--- src/color_math.rs
+++ src/color_math.rs
@@ -3,8 +3,9 @@ use std::cmp::{max, min};
 // TODO
 // [x] rewrite rgb_to_hsl() for new data types
 // [x] write test for rgb_to_hsl()
-// [] write tests for each region of hsl and hsb
-// [] change hex for new data types?
+// [x] write tests for each region of hsl and hsb
+// [x] change hex for new data types?
+// [] write test for rgb to hex
 
 #[derive(Debug)]
 pub enum Encoding {
@@ -107,7 +108,43 @@ impl Encoding {
         }
     }
 
-    fn rgb_to_hsl(&self) {}
+    fn rgb_to_hsl(&self) -> Encoding {
+        match self {
+            Encoding::Rgb(r, g, b) => {
+                let r = ((*r as f32 / 255.0) * 1000.0).round();
+                let g = ((*g as f32 / 255.0) * 1000.0).round();
+                let b = ((*b as f32 / 255.0) * 1000.0).round();
+                let bigger = max(r as i32, b as i32);
+                let smaller = min(r as i32, b as i32);
+                let c_max = max(bigger, g as i32) as f32;
+                let c_min = min(smaller, g as i32) as f32;
+                let delta = c_max - c_min;
+
+                let l = (c_max + c_min) / 2.0;
+
+                let h = if delta == 0.0 {
+                    0.0
+                } else if c_max == r {
+                    60.0 * ((g - b) / delta % 6.0)
+                } else if c_max == g {
+                    60.0 * ((b - r) / delta + 2.0)
+                } else if c_max == b {
+                    60.0 * ((r - g) / delta + 4.0)
+                } else {
+                    panic!("c_max ({c_max}) does not match r ({r}), g ({g}), or b ({b})");
+                };
+
+                let s = if delta == 0.0 {
+                    0.0
+                } else {
+                    delta / (1.0 - (2.0 * (l / 1000.0) - 1.0).abs())
+                };
+
+                Encoding::Hsl(h.round() as u16, s.round() as u16, l.round() as u16)
+            }
+            _ => panic!("wrong encoding type"),
+        }
+    }
     fn rgb_to_hsb(&self) -> Encoding {
         match self {
             Encoding::Rgb(r, g, b) => {
@@ -136,12 +173,20 @@ impl Encoding {
                     (delta / c_max) * 1000.0
                 };
                 let b = c_max;
-                return Encoding::Hsb(h.round() as u16, s.round() as u16, b.round() as u16);
+
+                Encoding::Hsb(h.round() as u16, s.round() as u16, b.round() as u16)
             }
             _ => panic!("wrong encoding type"),
         }
     }
-    fn rgb_to_hex(&self) {}
+    fn rgb_to_hex(&self) -> Encoding {
+        match self {
+            Encoding::Rgb(r, g, b) => {
+                Encoding::Hex((*r as u32) << 16 | (*g as u32) << 8 | (*b as u32))
+            }
+            _ => panic!("wrong encoding type"),
+        }
+    }
     fn rgb_to_name(&self) {}
 }
 
@@ -165,6 +210,13 @@ mod tests {
     #[test]
     fn hex_to_rgb() {
         let test = Encoding::Hex(0xf54927);
+        let result = test.translate_to_rgb();
+        assert_eq!(result, Encoding::Rgb(245, 73, 39));
+    }
+
+    #[test]
+    fn rgb_to_hex() {
+        let test = Encoding::Rgb(245, 73, 39);
         let result = test.translate_to_rgb();
         assert_eq!(result, Encoding::Rgb(245, 73, 39));
     }
@@ -222,10 +274,18 @@ mod tests {
     fn hsl_s_too_big() {
         Encoding::Hsl(10, 1001, 557).translate_to_rgb();
     }
+
     #[test]
     #[should_panic]
     fn hsl_l_too_big() {
         Encoding::Hsl(10, 912, 1001).translate_to_rgb();
+    }
+
+    #[test]
+    fn rgb_to_hsl() {
+        let test = Encoding::Rgb(245, 73, 39);
+        let result = test.rgb_to_hsl();
+        assert_eq!(result, Encoding::Hsl(10, 912, 557));
     }
 
     #[test]
